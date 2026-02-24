a149fa8678e024ba86b3b824039608ebbcfcd24f
maonaoda <maonaoda@gmail.com>
2026-02-24 16:15:24 +0800

    fix: add SelectDataTemplate for CV2

--- src/Controls/src/Core/Handlers/Items2/iOS/TemplatedCell2.cs
+++ src/Controls/src/Core/Handlers/Items2/iOS/TemplatedCell2.cs
@@ -217,10 +217,11 @@ namespace Microsoft.Maui.Controls.Handlers.Items2
 		public void Bind(DataTemplate template, object bindingContext, ItemsView itemsView)
 		{
 			View virtualView = null;
-			if (CurrentTemplate != template)
+			var itemTemplate = template.SelectDataTemplate(bindingContext, itemsView);
+			if (CurrentTemplate != itemTemplate)
 			{
-				CurrentTemplate = template;
-				virtualView = template.CreateContent(bindingContext, itemsView) as View;
+				CurrentTemplate = itemTemplate;
+				virtualView = itemTemplate.CreateContent(bindingContext, itemsView) as View;
 			}
 			else if (PlatformHandler?.VirtualView is View existingView)
 			{
@@ -267,7 +268,7 @@ namespace Microsoft.Maui.Controls.Handlers.Items2
 
 				virtualView.BindingContext = bindingContext;
 				itemsView.AddLogicalChild(virtualView);
-				
+
 				if (this.Selected)
 				{
 					UpdateVisualStates();
